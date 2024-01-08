25865fd26f02f3a30881daf4d011ec6a094b5a6b
inbouto <inbouto@protonmail.com>
2024-01-08 21:52:45 +0100

    updating for proper website

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://inbouto.github.io',
+  url: 'https://blueheartindustries.ddns.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/EveFleetSetups/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
