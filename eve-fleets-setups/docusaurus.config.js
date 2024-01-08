485d5cfed728fc360b6b13c7a34a385685f75b7b
inbouto <inbouto@protonmail.com>
2024-01-08 21:17:25 +0100

    uhhhhh

--- eve-fleets-setups/docusaurus.config.js
+++ eve-fleets-setups/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://${organisationName}.github.io',
+  url: 'https://inbouto.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/${projectName}/',
+  baseUrl: '/EveFleetSetups/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
